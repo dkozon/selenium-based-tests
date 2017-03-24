532f2389d3c1eabeb69c77e3d831d66a7c8db323
Dariusz Kozon <dariusz_kozon@epam.com>
2017-03-24 15:10:26 +0100

    add methods for operating on elements in selenium

--- src/test/java/net/kozon/selenium/example/test/framework/utils/CustomWait.java
+++ src/test/java/net/kozon/selenium/example/test/framework/utils/CustomWait.java
@@ -1,6 +1,7 @@
 package net.kozon.selenium.example.test.framework.utils;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,8 +10,6 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.sql.Time;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -31,8 +30,12 @@ public class CustomWait {
         return new WebDriverWait(webDriver, DEFAULT_TIMEOUT_IN_SEC);
     }
 
+    private FluentWait<WebDriver> createWait(final int seconds) {
+        return createFluentWait().withTimeout(seconds, SECONDS);
+    }
+
     public boolean isElementPresent(final WebElement element) {
-        FluentWait<WebDriver> wait = createFluentWait().withTimeout(DEFAULT_TIMEOUT_IN_SEC, SECONDS);
+        FluentWait<WebDriver> wait = createWait(DEFAULT_TIMEOUT_IN_SEC);
         try {
             wait.until(ExpectedConditions.visibilityOf(element));
             return true;
@@ -43,7 +46,7 @@ public class CustomWait {
     }
 
     public boolean isElementClicable(final WebElement element) {
-        FluentWait<WebDriver> wait = createFluentWait().withTimeout(DEFAULT_TIMEOUT_IN_SEC, SECONDS);
+        FluentWait<WebDriver> wait = createWait(DEFAULT_TIMEOUT_IN_SEC);
         try {
             wait.until(ExpectedConditions.elementToBeClickable(element));
             return true;
@@ -54,7 +57,7 @@ public class CustomWait {
     }
 
     public boolean isElementDisappear(final By by) {
-        FluentWait<WebDriver> wait = createFluentWait().withTimeout(DEFAULT_TIMEOUT_IN_SEC, SECONDS);
+        FluentWait<WebDriver> wait = createWait(DEFAULT_TIMEOUT_IN_SEC);
         try {
             wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
             return true;
@@ -62,5 +65,43 @@ public class CustomWait {
             logger.error("Element should not be visible!", e);
             return false;
         }
+    }
+
+    public void clickElement(final By by, final int timeoutInSeconds) {
+        FluentWait<WebDriver> wait = createWait(timeoutInSeconds);
+        wait.until(driver -> {
+            if(isElementClicable(driver.findElement(by))) {
+                driver.findElement(by).click();
+            }
+            return true;
+        });
+    }
+
+    public String getElementText(final By by, final int timeoutInSeconds) {
+        FluentWait<WebDriver> wait = createWait(timeoutInSeconds);
+        return wait.until(driver -> {
+            try {
+                if (isElementPresent(driver.findElement(by))) {
+                    return driver.findElement(by).getText();
+                }
+            } catch (NoSuchElementException e) {
+                logger.error("Element not found! Can't get element's text...", e);
+            }
+            return "";
+        });
+    }
+
+    public String getElementAttribute(final By by, final String attributeName, final int timeoutInSeconds) {
+        FluentWait<WebDriver> wait = createWait(timeoutInSeconds);
+        return wait.until(driver -> {
+            try {
+                if (!driver.findElement(by).getAttribute(attributeName).isEmpty()) {
+                    return driver.findElement(by).getAttribute(attributeName);
+                }
+            } catch (NoSuchElementException e) {
+                logger.error("Element not found! Can't get "+attributeName+"...", e);
+            }
+            return "";
+        });
     }
 }
