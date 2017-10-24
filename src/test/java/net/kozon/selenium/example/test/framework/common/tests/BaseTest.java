fe8cd7d341bab45c826274ce5f378f9cffa5d361
Dariusz Kozon <dariusz_kozon@epam.com>
2017-10-24 23:12:55 +0200

    new remotedriver

--- src/test/java/net/kozon/selenium/example/test/framework/common/tests/BaseTest.java
+++ src/test/java/net/kozon/selenium/example/test/framework/common/tests/BaseTest.java
@@ -4,10 +4,15 @@ import net.kozon.selenium.example.test.framework.common.utils.Configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.InvalidParameterException;
+
 
 /**
  * Created by Dariusz Kozon on 26.02.2017.
@@ -16,6 +21,7 @@ import java.security.InvalidParameterException;
  * Running with maven:
  * for Firefox use -Ddriver=firefox
  * for Chrome use -Ddriver=chrome
+ * for remote (establish server and node and then run) -Ddriver=remote
  *
  * If driver is not set then default configuration is Firefox (gecko driver).
  */
@@ -23,6 +29,7 @@ public class BaseTest {
 
     private static Logger logger = LoggerFactory.getLogger(BaseTest.class);
     private static final String DRIVER = "driver";
+    private static final String REMOTE_HOST_URL = "http://localhost:4444/wd/hub";
 
     protected WebDriver webDriver;
 
@@ -45,6 +52,13 @@ public class BaseTest {
         } else if (Configuration.getProperty(DRIVER).equals("firefox")) {
             System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
             webDriver = new FirefoxDriver();
+        } else if (Configuration.getProperty(DRIVER).equals("remote")) {
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            try {
+                webDriver = new RemoteWebDriver(new URL(REMOTE_HOST_URL), capabilities);
+            } catch (MalformedURLException e) {
+                logger.error("Missing RemoteWebDriver! ", e);
+            }
         }
     }
 }
--- src/test/java/net/kozon/selenium/example/test/framework/internet/tests/DragAndDropTest.java
+++ src/test/java/net/kozon/selenium/example/test/framework/internet/tests/DragAndDropTest.java
@@ -3,7 +3,6 @@ package net.kozon.selenium.example.test.framework.internet.tests;
 import net.kozon.selenium.example.test.framework.common.tests.BaseTest;
 import net.kozon.selenium.example.test.framework.common.utils.PageObjectTheInternetManager;
 import net.kozon.selenium.example.test.framework.common.utils.UrlProvider;
-import org.junit.Ignore;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
