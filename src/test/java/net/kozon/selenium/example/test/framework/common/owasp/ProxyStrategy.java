3302a8cf290ae2c8c1e67c0827579555f037a779
Dariusz Kozon <dariusz_kozon@epam.com>
2017-11-13 11:27:36 +0100

    trivial changes

--- src/test/java/net/kozon/selenium/example/test/framework/common/owasp/ProxyStrategy.java
+++ src/test/java/net/kozon/selenium/example/test/framework/common/owasp/ProxyStrategy.java
@@ -8,9 +8,7 @@ import java.io.IOException;
  * Created by Dariusz_Kozon on 06.11.2017.
  */
 public interface ProxyStrategy {
-
     String PROXY = "localhost";
     int PORT = 8081;
     WebDriver webDriver() throws IOException;
-
 }
