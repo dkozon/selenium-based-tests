142d11f85db083a640bcaf4553185defd08f759e
Dariusz Kozon <dariusz@kozon.net>
2018-03-21 16:10:00 +0100

    minor changes

---
+++ src/test/java/net/kozon/selenium/example/test/framework/common/grid/TestOnGrid.java
@@ -0,0 +1,63 @@
+package net.kozon.selenium.example.test.framework.common.docker;
+
+import org.openqa.grid.common.RegistrationRequest;
+import org.openqa.grid.internal.utils.SelfRegisteringRemote;
+import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
+import org.openqa.grid.internal.utils.configuration.GridNodeConfiguration;
+import org.openqa.grid.shared.GridNodeServer;
+import org.openqa.grid.web.Hub;
+import org.openqa.selenium.remote.server.SeleniumServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestOnGrid {
+
+    private static Logger logger = LoggerFactory.getLogger(TestOnGrid.class);
+
+    private static Hub hub;
+    private static GridNodeServer node;
+    private static SelfRegisteringRemote remote;
+
+    public TestOnGrid() {
+        hub = new Hub(GridHubConfiguration.loadFromJSON("src/test/resources/selenium-grid-hub-config.json"));
+        RegistrationRequest registrationRequest = new RegistrationRequest(GridNodeConfiguration.loadFromJSON("src/test/resources/selenium-grid-node-config.json"));
+        node = new SeleniumServer(registrationRequest.getConfiguration());
+        remote = new SelfRegisteringRemote(registrationRequest);
+    }
+
+    public boolean runHub() {
+        try {
+            hub.start();
+            return true;
+        } catch (Exception e) {
+            logger.error("Cannot establish Selenium Grid HUB ", e);
+            return false;
+        }
+    }
+
+    public boolean stopHub() {
+        try {
+            hub.stop();
+            return true;
+        } catch (Exception e) {
+            logger.error("Cannot stop Selenium Grid HUB ", e);
+            return false;
+        }
+    }
+
+    public boolean runNode() {
+        remote.setRemoteServer(node);
+        try {
+            remote.startRemoteServer();
+            remote.startRegistrationProcess();
+            return true;
+        } catch (Exception e) {
+            logger.error("Cannot establish Selenium Grid NODE", e);
+            return false;
+        }
+    }
+
+    public void stopNode() {
+        remote.stopRemoteServer();
+    }
+}
