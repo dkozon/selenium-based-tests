8acaf49fff9fd6268b11fe18bacfc41519fc89de
Dariusz Kozon <dariusz@kozon.net>
2018-07-23 17:40:59 +0200

    fixing failing test

--- src/test/java/net/kozon/selenium/example/test/framework/niebezpiecznik/pages/TigerBonzoHackerPage.java
+++ src/test/java/net/kozon/selenium/example/test/framework/niebezpiecznik/pages/TigerBonzoHackerPage.java
@@ -14,10 +14,10 @@ public class TigerBonzoHackerPage extends BasePage<TigerBonzoHackerPage> {
     @FindBy(xpath = ".//form/input[@class='tiger_przepraszam']")
     private WebElement tigerBonzoQuestionField;
 
-    @FindBy(xpath = "/html/body/div/div[3]/div/div[3]/div[1]/form/input[2]")
+    @FindBy(xpath = "/html/body/div/div[3]/div/div[3]/div[1]/form/input[2]") //example of non optimized xpath
     private WebElement submitButton;
 
-    private String infoSentMessage = "/html/body/div/div[3]/div/div[3]/div[2]/p/span[contains(text('%s'))]";
+    private String infoSentMessage = "//span[contains(text(),'%s')]";
 
     public TigerBonzoHackerPage(WebDriver webDriver) {
         super(webDriver);
